3ba7685b4e2859eb29d4f193e47f928a655abf3a
Victor Torres <vpaivatorres@gmail.com>
2014-09-11 13:11:14 -0300

    cadastro automatico dos privilegios e usuario admin caso ele não exista

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,31 @@
+import org.natalnet.weduc.Usuario
+import org.natalnet.weduc.Privilegio
+import org.natalnet.weduc.UsuarioPrivilegio
+
 class BootStrap {
 
     def init = { servletContext ->
+    	// Registra privilégios, caso não existam
+    	def privilegios = ["ADMIN", "PROFESSOR", "ALUNO"]
+    	privilegios.each {
+    		Privilegio.findOrSaveWhere(authority: it)
+    	}
+
+    	// Registra usuário padrão
+    	def privilegio = Privilegio.findWhere(authority: "ADMIN")
+    	def usuario = Usuario.findWhere(username: "admin")
+    	if(!usuario) {
+    		usuario = new Usuario()
+    		usuario.username = "admin"
+    		usuario.password = "admin"
+    		usuario.email = "admin@email.com"
+    		usuario.save flush: true, failOnError: true
+
+    		def usuarioPrivilegio = new UsuarioPrivilegio()
+    		usuarioPrivilegio.usuario = usuario
+    		usuarioPrivilegio.privilegio = privilegio
+    		usuarioPrivilegio.save flush: true, failOnError: true
+    	}
     }
     def destroy = {
     }
