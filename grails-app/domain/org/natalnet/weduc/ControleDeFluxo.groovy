0b89fa43a284a6daa4abe91f355e10a4e1f70636
Victor Torres <vpaivatorres@gmail.com>
2014-10-11 09:55:33 -0300

    controle de fluxo

---
+++ grails-app/domain/org/natalnet/weduc/ControleDeFluxo.groovy
@@ -0,0 +1,16 @@
+package org.natalnet.weduc
+
+class ControleDeFluxo {
+    
+    String languageName
+    String breakCode
+    String doCode
+    String forCode
+    String ifCode
+    String repeatCode
+    String switchCode
+    String whileCode
+
+    static constraints = {
+    }
+}
