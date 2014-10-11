4c73acea59aaa80f1971c92e42e5b1e5e252bd3e
Victor Torres <vpaivatorres@gmail.com>
2014-10-11 09:56:03 -0300

    linguagem

---
+++ grails-app/domain/org/natalnet/weduc/Linguagem.groovy
@@ -0,0 +1,28 @@
+package org.natalnet.weduc
+
+class Linguagem {
+    
+    String name
+    String description
+    String robot
+    String extension
+    String compileCode
+    String compilerFile
+    String sendCode
+    String sentExtension
+    String header
+    String footnote
+    String mainFunction
+    String otherFunctions
+    String callFunction
+    
+    Tipos types
+    Operadores operators
+    ControleDeFluxo controlFlow
+    
+    static hasMany = [functions: Funcao, defines: Definicao]
+
+    static constraints = {
+    }
+    
+}
