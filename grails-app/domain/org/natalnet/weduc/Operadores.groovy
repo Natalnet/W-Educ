a7c1ae95595f9e7d091925e7d8580df83b4c4b14
Victor Torres <vpaivatorres@gmail.com>
2014-10-11 09:56:13 -0300

    operadores

---
+++ grails-app/domain/org/natalnet/weduc/Operadores.groovy
@@ -0,0 +1,18 @@
+package org.natalnet.weduc
+
+class Operadores {
+    
+    String name
+    String equalTo
+    String notEqualTo
+    String greaterThan
+    String lessThan
+    String greaterThanOrEqualTo
+    String lessThanOrEqualTo
+    String logicalAnd
+    String logicalOr
+    String logicalNot
+
+    static constraints = {
+    }
+}
