df79965c0362c0ddce6bd1ae3c4d6d5a380712e5
sarinhaxp <sarinhaxp@gmail.com>
2015-10-21 18:35:29 -0300

    -

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-grails.server.port.http = 9090
+grails.server.port.http = 8080
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
