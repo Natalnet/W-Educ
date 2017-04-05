cbfcdc8ada386c74927a002862cd8fec9566e7d0
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2017-04-05 11:40:16 -0300

    Utilizar apenas últimas 3 mensagens

--- grails-app/conf/MensagemFilters.groovy
+++ grails-app/conf/MensagemFilters.groovy
@@ -2,15 +2,20 @@ import org.natalnet.weduc.Mensagem;
 
 class MensagemFilters {
 
-	def springSecurityService
-        
-	def filters = {
-		ultimasMensagens(controller: '*', action: '*') {
-			before = { model ->
-				def ultimasMensagens = Mensagem.findAllWhere(destinatario: springSecurityService.getCurrentUser())
-                                //def ultimasMensagens = Mensagem.list(max: 3, fetch: [destinatario: springSecurityService.getCurrentUser()])
-				request.setAttribute("ultimasMensagens", ultimasMensagens)
-			}
-		}
-	}
+    def springSecurityService
+
+    def filters = {
+        ultimasMensagens(controller: '*', action: '*') {
+            before = { model ->
+                //def ultimasMensagens = Mensagem.findAllWhere(destinatario: springSecurityService.getCurrentUser())
+                def usuario = springSecurityService.getCurrentUser()
+                def ultimasMensagens = Mensagem.withCriteria {
+                    eq('destinatario', usuario)
+                    order('data', 'desc')
+                    maxResults(3)
+                }
+                request.setAttribute("ultimasMensagens", ultimasMensagens)
+            }
+        }
+    }
 }
